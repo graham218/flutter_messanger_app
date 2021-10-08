9864a49537cb72efad9c5474c13e0c270420cc01
BILL GRAHAM <grahambill011@gmail.com>
2021-10-09 01:08:37 +0300

    Update signin.dart

---
+++ flutter_messenger/lib/views/signin.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SignIN extends StatefulWidget {
+  const SignIN({Key? key}) : super(key: key);
+
+  @override
+  _SignINState createState() => _SignINState();
+}
+
+class _SignINState extends State<SignIN> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
